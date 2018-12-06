57aa6e4f05a8b8c060f49b716d4c7057b9121fad
zqqiang <qiangzhaoqing@gmail.com>
2018-12-06 09:38:05 -0800

    add css

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
-require("assets/css/material-dashboard.min.css");
-require("bootstrap-material-design");
+require("assets/css/bootstrap.min.css");
+require("assets/css/mdb.min.css");
+require("assets/css/style.min.css");
 require("components/site-layout");
 require("marko/components").init();
